be91e82444db7aa550eeae0366dfad9d9ea228b0
Drelofs <ali.shahid.1994@gmail.com>
2020-06-11 01:43:58 +0200

    Added gateways and fixed door bug

--- Entities/Objects/Gateway/door.gd
+++ Entities/Objects/Gateway/door.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-export(PackedScene) var go_to 		# De bestemmingslocatie
+export var go_to = "" 		# De bestemmingslocatie
 export var with_player = true 		# Of de speler op de bestemming moet verschijnen
 export var player_x = 0 			# locatie van speler op bestemming
 export var player_y = 0 			# locatie van speler op bestemming
@@ -12,5 +12,4 @@ func _on_Gateway_input_event(_viewport, event, _shape_idx):
 	if event.is_action_pressed('left_click') && !locked && go_to:
 		if Global.active_player:
 			if !Global.active_player.conversation_partner:
-				var _path = go_to.get_path()
-				Global.go_to_level( _path, with_player, player_x, player_y )
+				Global.go_to_level(go_to, with_player, player_x, player_y )
